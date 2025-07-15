2f18d35951a0977222d6db3cfcb28f559b41ff70
Manish Sharma <kandansuryamass@gmail.com>
2025-07-15 16:43:45 +0530

    added .env file and configred meta data

---
+++ lib/constants/index.ts
@@ -0,0 +1,3 @@
+export const APP_NAME = process.env.NEXT_PUBLIC_APP_NAME || 'PROSTORE'
+export const APP_DESCRIPTION = process.env.NEXT_PUBLIC_APP_DESCRIPTION || 'i oNLY sEE MY NAME'
+export const APP_SERVER_URL = process.env.NEXT_PUBLIC_APP_SERVER_URL || 'http://localhost:3000'
